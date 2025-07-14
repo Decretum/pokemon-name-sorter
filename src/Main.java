74bb0470dfed1675d87a49cb968efc272307c5b4
George <georgezmsje@yahoo.com>
2025-07-14 14:05:57 -0700

    Got it working

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,230 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    public static String INPUT = """
+Bulbasaur
+Ivysaur
+Venusaur
+Charmander
+Charizard
+Squirtle
+Blastoise
+Rattata
+Pikachu
+Sandshrew
+Nidoran F
+Nidoqueen
+Nidoran M
+Diglett
+Psyduck
+Golduck
+Growlithe
+Arcanine
+Poliwrath
+Alakazam
+Machop
+Machoke
+Ponyta
+Magnemite
+Magneton
+Shellder
+Cloyster
+Gengar
+Onix
+Exeggcute
+Koffing
+Rhyhorn
+Rhydon
+Chansey
+Kangaskhan
+Pinsir
+Magikarp
+Gyarados
+Eevee
+Jolteon
+Flareon
+Aerodactyl
+Snorlax
+Articuno
+Zapdos
+Moltres
+Dratini
+Dragonair
+Mewtwo
+Mew
+Totodile
+Croconaw
+Feraligatr
+Hoothoot
+Crobat
+Togepi
+Ampharos
+Wooper
+Slowking
+Unown
+Scizor
+Sneasel
+Delibird
+Skarmory
+Porygon2
+Smeargle
+Blissey
+Raikou
+Entei
+Suicune
+Lugia
+Ho-oh
+Torchic
+Blaziken
+Poochyena
+Linoone
+Shiftry
+Ralts
+Gardevoir
+Surskit
+Shedinja
+Mawile
+Aggron
+Roselia
+Spoink
+Trapinch
+Barboach
+Dusclops
+Absol
+Glalie
+Sealeo
+Clamperl
+Bagon
+Salamence
+Beldum
+Metagross
+Regice
+Registeel
+Latias
+Latios
+Groudon
+Rayquaza
+Jirachi
+Deoxys
+Chimchar
+Monferno
+Kricketot
+Kricketune
+Luxray
+Combee
+Floatzel
+Ambipom
+Bonsly
+Lucario
+Drapion
+Magnezone
+Mamoswine
+Gallade
+Froslass
+Rotom
+Azelf
+Dialga
+Palkia
+Heatran
+Giratina
+Cresselia
+Manaphy
+Tepig
+Emboar
+Oshawott
+Blitzle
+Gigalith
+Oddish
+Audino
+Conkeldurr
+Lilligant
+Vanillite
+Cubchoo
+Shelmet
+Hydreigon
+Reshiram
+Zekrom
+Kyurem
+Meloetta
+Greninja
+Talonflame
+Gogoat
+Aegislash
+Avalugg
+Xerneas
+Zygarde
+Rockruff
+Tsareena
+Silvally
+Mimikyu
+Kommo-o
+Tapu Lele
+Tapu Bulu
+Celesteela
+Blacephalon
+Meltan
+Corviknight
+Applin
+Toxel
+Hatterene
+Duraludon
+Enamorus
+Lechonk
+Oinkologne
+Nymble
+Revavroom
+Orthworm
+Dondozo
+Scream Tail
+Flutter Mane
+Dipplin
+            """;
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<String> sorted = sort(INPUT);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+        for (String s : sorted) {
+            System.out.println(s);
         }
+    }
+
+    public static List<String> sort(String unsorted) {
+        String[] pokemon = AllPokemon.POKEMON.split("\n");
+        String[] input = unsorted.split("\n");
+
+        ArrayList<String> sorted = new ArrayList<>();
+        for (String entry : input) {
+            if (!entry.isBlank()) {
+                int dexNumber = getDexNumber(entry, pokemon);
+
+                boolean added = false;
+                for (int x = 0; x < sorted.size(); x++) {
+                    if (!added) {
+                        if (dexNumber < getDexNumber(sorted.get(x), pokemon)) {
+                            sorted.add(x, entry);
+                            added = true;
+                        }
+                    }
+                }
+                if (!added) {
+                    sorted.add(entry);
+                }
+            }
+        }
+
+        return sorted;
+    }
+
+    private static int getDexNumber(String name, String[] pokemon) {
+        for (int x = 1; x <= pokemon.length; x++) {
+            if (name.trim().equalsIgnoreCase(pokemon[x - 1].trim())) {
+                return x;
+            }
+        }
+        throw new RuntimeException("Unknown Pokemon: " + name);
     }
 }
--- test/MainTest.java
+++ test/MainTest.java
@@ -1,4 +1,53 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
-class MainTest {
-  
+
+public class MainTest {
+    @Test
+    public void name() {
+        String input = """
+                Onix
+                Blacephalon
+                Sandshrew
+                Sealeo
+                Poochyena
+                Luxray
+                Shelmet
+                Koffing
+                Rhydon
+                Jolteon
+                Flareon
+                Dratini
+                Rattata
+                Nidoqueen
+                Diglett
+                Growlithe
+                Machoke
+                Dragonair
+                Totodile
+                Croconaw
+                Feraligatr
+                Togepi
+                """;
+        Set<String> sorted = new HashSet<>(Main.sort(input));
+        Set<String> unsorted = Arrays.stream(input.split("\n")).filter(s -> !s.isBlank()).collect(Collectors.toSet());
+
+        // After set deduping, check that no elements were added ore removed
+        assertEquals(sorted.size(), unsorted.size());
+
+        // Check that each element is accounted for
+        for (String s : sorted) {
+            assertTrue(unsorted.contains(s));
+        }
+        for (String s : unsorted) {
+            assertTrue(sorted.contains(s));
+        }
+    }
+
+
 }
